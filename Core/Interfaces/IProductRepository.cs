ae97733d8f27d69e2d3e5b6e8e130e82b295f2ad
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2021-03-03 12:32:22 +0300

    Migrations changed

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+         
+    }
+}
