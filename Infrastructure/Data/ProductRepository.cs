b5a44520bd812d7b87002ed8fd06c7546d951e66
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2021-03-09 15:19:22 +0300

    GET methods edited

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -22,13 +22,19 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+            .Include(p=>p.ProductType)
+            .Include(p=>p.ProductBrand)
+            .FirstOrDefaultAsync(p=>p.Id==id);
 
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+            .Include(p=>p.ProductType)
+            .Include(p=>p.ProductBrand)
+            .ToListAsync();
 
         }
 
