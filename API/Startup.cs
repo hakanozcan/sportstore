d705d82e65b9314dce3d53695fa3593797375e03
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2021-06-27 15:05:04 +0300

    Added Generic Repository

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,7 +34,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IProductRepository, ProductRepository>();
-
+            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
