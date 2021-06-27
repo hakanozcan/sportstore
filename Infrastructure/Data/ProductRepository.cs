3af7e022d31ff450cb41d829c6de59e8189b1894
Hakan Özcan <hakanozcan20@hotmail.com.tr>
2021-06-27 23:49:39 +0300

    Spesifications created

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
@@ -31,6 +32,7 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+           
             return await _context.Products
             .Include(p=>p.ProductType)
             .Include(p=>p.ProductBrand)
